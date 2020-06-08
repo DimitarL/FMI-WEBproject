e153d0de7327baa8d5a7117f7f37f4675310cb45
aprazova <alexandra.prazova@gmail.com>
2020-06-08 11:00:28 +0300

    Adding hardcoded data to db

---
+++ SQL/lectors.sql
@@ -0,0 +1,14 @@
+INSERT INTO lectors (firstName, lastName, username, password)
+VALUES ('Иван', 'Василев', 'ivvasilev', '$2y$10$/Xc4cPsrd4WoB7UWzzXc.OPx..tuX1GNoWNmzcCGyZN33C59hGtkK'); --pass = ivvasilev
+
+INSERT INTO lectors (firstName, lastName, username, password)
+VALUES ('Марио', 'Миков', 'mamikov', '$2y$10$8k/gxdL2kqfs..2Mm1seger6oJKdjT3nMJaSkPT/LMx2KKf.Nqdsy'); --pass = mamikov
+
+INSERT INTO lectors (firstName, lastName, username, password)
+VALUES ('Георги', 'Георгиев', 'gegeorgiev', '$2y$10$2dH4E82ZVXL0gyqkMWmCeu1GyNI.m.0kKDfJhhD1lVIK5euJ2gaEa'); --pass = gegeorgiev
+
+INSERT INTO lectors (firstName, lastName, username, password)
+VALUES ('Симона', 'Генева', 'sigeneva', '$2y$10$/WMDqzAoYE..Omp5KbV1be8PlF5S9/.r9BitBrAkYiXmKAv8cue/S'); --pass = sigeneva
+
+INSERT INTO lectors (firstName, lastName, username, password)
+VALUES ('Мартина', 'Минева', 'mamineva', '$2y$10$okneBzAVTXaiUA/Bqq21YuNvRY.LRy82naVdrw1vEOSJw9vEDIO/C'); --pass = mamineva
