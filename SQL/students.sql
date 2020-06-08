51266630f0f1598c051ed74c7ab7000bd6d5ec6b
aprazova <alexandra.prazova@gmail.com>
2020-06-08 11:28:19 +0300

    Changing comments type

--- SQL/students.sql
+++ SQL/students.sql
@@ -1,44 +1,44 @@
 INSERT INTO students(firstName, lastName, username, password, course, specialty, facultyNumber, groupNumber)
-VALUES ('Александър', 'Симеонов', 'asimeonov', '$2y$10$Yy.RsKq07b4JfL.krjJbvu4JwOV.DPQwMm2fFEEWJUT.XvGxub1ze', 3, 'Софтуерно инженерство', 78412, 1); --pass = asimeonov
+VALUES ('Александър', 'Симеонов', 'asimeonov', '$2y$10$Yy.RsKq07b4JfL.krjJbvu4JwOV.DPQwMm2fFEEWJUT.XvGxub1ze', 3, 'Софтуерно инженерство', 78412, 1); /*pass = asimeonov*/
 
 INSERT INTO students(firstName, lastName, username, password, course, specialty, facultyNumber, groupNumber)
-VALUES ('Мартина', 'Александрова', 'maleksandrova', '$2y$10$AYdC1asECjJwNQYpQkgAnOp5R0qlbnuDDRlG5f/1daQeclvVSnXMO', 3, 'Софтуерно инженерство', 78416, 1); --pass = maleksandrova
+VALUES ('Мартина', 'Александрова', 'maleksandrova', '$2y$10$AYdC1asECjJwNQYpQkgAnOp5R0qlbnuDDRlG5f/1daQeclvVSnXMO', 3, 'Софтуерно инженерство', 78416, 1); /*pass = maleksandrova*/
 
 INSERT INTO students(firstName, lastName, username, password, course, specialty, facultyNumber, groupNumber)
-VALUES ('Василена', 'Иванова', 'vivanova', '$2y$10$vvXsifXAHiHp35dcpOV8eOBIPN0KGbtZX2QS25.o7ZVDO9aQ8PTIC', 3, 'Софтуерно инженерство', 78417, 2); --pass = vivanova
+VALUES ('Василена', 'Иванова', 'vivanova', '$2y$10$vvXsifXAHiHp35dcpOV8eOBIPN0KGbtZX2QS25.o7ZVDO9aQ8PTIC', 3, 'Софтуерно инженерство', 78417, 2); /*pass = vivanova*/
 
 INSERT INTO students(firstName, lastName, username, password, course, specialty, facultyNumber, groupNumber)
-VALUES ('Виктор', 'Мишев', 'vmishev', '$2y$10$bl8Q.M6TcSSdJz9smTHbzuXN0AvgFzXc5ZL7CzQ.GHWtMrD4xYSdW', 3, 'Софтуерно инженерство', 78419, 2); --pass = vmishev
+VALUES ('Виктор', 'Мишев', 'vmishev', '$2y$10$bl8Q.M6TcSSdJz9smTHbzuXN0AvgFzXc5ZL7CzQ.GHWtMrD4xYSdW', 3, 'Софтуерно инженерство', 78419, 2); /*pass = vmishev*/
 
 INSERT INTO students(firstName, lastName, username, password, course, specialty, facultyNumber, groupNumber)
-VALUES ('Ивана', 'Симеонова', 'isimeonova', '$2y$10$qw3Y60GJaJ/9hzzhLqtuQu33Mw4qJg7c5raA2cdaqYTSg4IJyZ2bC', 3, 'Софтуерно инженерство', 78502, 1); --pass = isimeonova
+VALUES ('Ивана', 'Симеонова', 'isimeonova', '$2y$10$qw3Y60GJaJ/9hzzhLqtuQu33Mw4qJg7c5raA2cdaqYTSg4IJyZ2bC', 3, 'Софтуерно инженерство', 78502, 1); /*pass = isimeonova*/
 
 INSERT INTO students(firstName, lastName, username, password, course, specialty, facultyNumber, groupNumber)
-VALUES ('Иван', 'Иванов', 'iivanov', '$2y$10$FDu9vwVDnamTHepOhanNAOKkURtH44VfuZhOlQvtB5ZRiIm0WzLjS', 3, 'Софтуерно инженерство', 78423, 2); --pass = iivanov
+VALUES ('Иван', 'Иванов', 'iivanov', '$2y$10$FDu9vwVDnamTHepOhanNAOKkURtH44VfuZhOlQvtB5ZRiIm0WzLjS', 3, 'Софтуерно инженерство', 78423, 2); /*pass = iivanov*/
 
 INSERT INTO students(firstName, lastName, username, password, course, specialty, facultyNumber, groupNumber)
-VALUES ('Маргарита', 'Менчева', 'mmencheva', '$2y$10$5094juCXnLn3O9oLm3YQYOPT3y1jJRyLus1wQQdJ0MkULYz.214Cy', 3, 'Софтуерно инженерство', 78432, 2); --pass = mmencheva
+VALUES ('Маргарита', 'Менчева', 'mmencheva', '$2y$10$5094juCXnLn3O9oLm3YQYOPT3y1jJRyLus1wQQdJ0MkULYz.214Cy', 3, 'Софтуерно инженерство', 78432, 2); /*pass = mmencheva*/
 
 INSERT INTO students(firstName, lastName, username, password, course, specialty, facultyNumber, groupNumber)
-VALUES ('Милен', 'Алексиев', 'maleksiev', '$2y$10$UfcHFpuMqCwhBzQVc3cNeOCBegLSiPCL14QBGSts07DXDkWka8Jwu', 3, 'Софтуерно инженерство', 78433, 2); --pass = maleksiev
+VALUES ('Милен', 'Алексиев', 'maleksiev', '$2y$10$UfcHFpuMqCwhBzQVc3cNeOCBegLSiPCL14QBGSts07DXDkWka8Jwu', 3, 'Софтуерно инженерство', 78433, 2); /*pass = maleksiev*/
 
 INSERT INTO students(firstName, lastName, username, password, course, specialty, facultyNumber, groupNumber)
-VALUES ('Доротея', 'Димитрова', 'dodimitrova', '$2y$10$/GMLOjU/BuoimSqX.lNywurwAPtYVh7VcF093Fh3NaOQboQ48lhAW', 3, 'Софтуерно инженерство', 78464, 1); --pass = dodimitrova
+VALUES ('Доротея', 'Димитрова', 'dodimitrova', '$2y$10$/GMLOjU/BuoimSqX.lNywurwAPtYVh7VcF093Fh3NaOQboQ48lhAW', 3, 'Софтуерно инженерство', 78464, 1); /*pass = dodimitrova*/
 
 INSERT INTO students(firstName, lastName, username, password, course, specialty, facultyNumber, groupNumber)
-VALUES ('Дейвид', 'Денков', 'ddenkov', '$2y$10$7bzbODfsGoTlmee.279wOuqfdfS4PXBrHMP6pWcd5o.KhfHKwPqGG', 3, 'Софтуерно инженерство', 78468, 2); --pass = ddenkov
+VALUES ('Дейвид', 'Денков', 'ddenkov', '$2y$10$7bzbODfsGoTlmee.279wOuqfdfS4PXBrHMP6pWcd5o.KhfHKwPqGG', 3, 'Софтуерно инженерство', 78468, 2); /*pass = ddenkov*/
 
 INSERT INTO students(firstName, lastName, username, password, course, specialty, facultyNumber, groupNumber)
-VALUES ('Цветелина', 'Нончева', 'cvnoncheva', '$2y$10$TRmuH4gwfOq7/v5vh//Wl.WYMYe450NNRjjdNLdZxw9qawkJny7fG', 3, 'Софтуерно инженерство', 78481, 1); --pass = cvnoncheva
+VALUES ('Цветелина', 'Нончева', 'cvnoncheva', '$2y$10$TRmuH4gwfOq7/v5vh//Wl.WYMYe450NNRjjdNLdZxw9qawkJny7fG', 3, 'Софтуерно инженерство', 78481, 1); /*pass = cvnoncheva*/
 
 INSERT INTO students(firstName, lastName, username, password, course, specialty, facultyNumber, groupNumber)
-VALUES ('Симон', 'Генчев', 'sgenchev', '$2y$10$4EUuhL/lIk//0i5vJZRVGOCbmJBPlxybrP4Fcq9/hd3e8lbj8TBSi', 3, 'Софтуерно инженерство', 78482, 2); --pass = sgenchev
+VALUES ('Симон', 'Генчев', 'sgenchev', '$2y$10$4EUuhL/lIk//0i5vJZRVGOCbmJBPlxybrP4Fcq9/hd3e8lbj8TBSi', 3, 'Софтуерно инженерство', 78482, 2); /*pass = sgenchev*/
 
 INSERT INTO students(firstName, lastName, username, password, course, specialty, facultyNumber, groupNumber)
-VALUES ('Мартинела', 'Михалкова', 'mmihalkova', '$2y$10$CCGb8CpVt/nxzFADbFH2DOVVOg8vO4TAEiG1yheR6L1bzUlAzGzse', 3, 'Софтуерно инженерство', 78489, 1); --pass = mmihalkova
+VALUES ('Мартинела', 'Михалкова', 'mmihalkova', '$2y$10$CCGb8CpVt/nxzFADbFH2DOVVOg8vO4TAEiG1yheR6L1bzUlAzGzse', 3, 'Софтуерно инженерство', 78489, 1); /*pass = mmihalkova*/
 
 INSERT INTO students(firstName, lastName, username, password, course, specialty, facultyNumber, groupNumber)
-VALUES ('Йордан', 'Цончев', 'yochonchev', '$2y$10$XdPIr1eWcjPoRg0dmpg9oOxcd7Q6S5Qj.iRxrxYI315hdt3c6E4XG', 3, 'Софтуерно инженерство', 78490, 1); --pass = yochonchev
+VALUES ('Йордан', 'Цончев', 'yochonchev', '$2y$10$XdPIr1eWcjPoRg0dmpg9oOxcd7Q6S5Qj.iRxrxYI315hdt3c6E4XG', 3, 'Софтуерно инженерство', 78490, 1); /*pass = yochonchev*/
 
 INSERT INTO students(firstName, lastName, username, password, course, specialty, facultyNumber, groupNumber)
-VALUES ('Здравко', 'Конев', 'zkonev', '$2y$10$WTkenPrghpJKzukTEZmTYu7hezyLzSx5UwumK.wSKlqfMo9Vq0dxW', 3, 'Софтуерно инженерство', 78497, 1); --pass = zkonev
+VALUES ('Здравко', 'Конев', 'zkonev', '$2y$10$WTkenPrghpJKzukTEZmTYu7hezyLzSx5UwumK.wSKlqfMo9Vq0dxW', 3, 'Софтуерно инженерство', 78497, 1); /*pass = zkonev*/
