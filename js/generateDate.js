56c690bd3ab1decb565813d326c1ce8efe400849
dkrusteva1 <dessynceto@abv.bg>
2020-06-11 15:09:56 +0300

    Add function to check if start time is less than end time

--- js/generateDate.js
+++ js/generateDate.js
@@ -12,7 +12,7 @@ if (mm < 10) {
 today = yyyy + '-' + mm + '-' + dd;
 document.getElementById("date").setAttribute("min", today);
 
-function validateHour(inputField) {
+function validateStart(inputField) {
     var isValid = /^([0-1]?[0-9]|2[0-4]):([0-5][0-9])(:[0-5][0-9])?$/.test(inputField.value);
 
     if (isValid) {
@@ -22,4 +22,38 @@ function validateHour(inputField) {
     }
 
     return isValid;
+}
+
+function validateEnd(inputField) {
+    var isValid = /^([0-1]?[0-9]|2[0-4]):([0-5][0-9])(:[0-5][0-9])?$/.test(inputField.value);
+    if (isValid && compare(document.getElementById("start").value, inputField.value)) {
+        inputField.style.backgroundColor = '#bfa';
+    } else {
+        inputField.style.backgroundColor = '#fba';
+    }
+    return isValid;
+}
+
+function compare(start, end) {
+
+    var startTime = new Date().setHours(getHours(start), getMinutes(start), 0);
+    var endTime = new Date(startTime);
+    endTime = endTime.setHours(getHours(end), getMinutes(end), 0);
+    if (startTime > endTime) {
+        return false;
+    }
+    if (startTime == endTime) {
+        return false;
+    }
+    if (startTime < endTime) {
+        return true;
+    }
+}
+
+function getHours(d) {
+    return h = parseInt(d.split(':')[0]);
+}
+
+function getMinutes(d) {
+    return parseInt(d.split(':')[1].split(' ')[0]);
 }
