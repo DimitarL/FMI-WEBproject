cb85ec31c93e5e02522476382a9bc98a0b2fcb31
aprazova <alexandra.prazova@gmail.com>
2020-06-24 17:08:01 +0300

    Adding validation for present button

--- js/get_present.js
+++ js/get_present.js
@@ -1,19 +1,39 @@
 import { ajax_json } from './ajax.js';
 
-document.getElementById('getFile').addEventListener('click', function() {
-    let callback = function(data) {
-        // console.log(data);
+let isLetor;
 
-        let a = document.createElement('a');
-        let url = window.URL.createObjectURL(new Blob([data]));
-        a.href = url;
-        a.download = 'present.txt';
-        document.body.append(a);
-        a.click();
-        a.remove();
-        window.URL.revokeObjectURL(url);
-        // console.log("END");
+isLector();
+
+if (isLector) {
+    let btn = document.createElement("BUTTON");
+    btn.innerHTML = "Спистък с присъстващи";
+    btn.id = "getFile";
+    document.getElementById("presentButton").appendChild(btn);
+
+    document.getElementById('getFile').addEventListener('click', function() {
+        let callback = function(data) {
+            let a = document.createElement('a');
+            let url = window.URL.createObjectURL(new Blob([data]));
+            a.href = url;
+            a.download = 'present.txt';
+            document.body.append(a);
+            a.click();
+            a.remove();
+            window.URL.revokeObjectURL(url);
+        }
+        ajax_json("GET", "../php/get_present.php", { success: callback });
+    })
+} else {
+    console.log("student");
+}
+
+function isLector() {
+    let callback = function(msg) {
+        if (msg == "1") {
+            isLector = true;
+        } else {
+            isLector = false;
+        }
     }
-    ajax_json("GET", "../php/get_present.php", { success: callback });
-
-})
+    ajax_json("GET", "../php/is_lector.php", { success: callback });
+}
