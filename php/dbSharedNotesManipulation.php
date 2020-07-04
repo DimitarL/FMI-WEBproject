6b52eac3381d8c5564b74cbeb8abb5f356345c1a
DimitarL <miteto66@gmail.com>
2020-07-04 17:29:11 +0300

    Added note's time and write's username

--- php/dbSharedNotesManipulation.php
+++ php/dbSharedNotesManipulation.php
@@ -1,18 +1,25 @@
 <?php
+if(!session_id()){
+    session_start();
+}
+
 include "db_connection.php";
 
 $table_name = "sharedNotes";
 
 function insertInTable($inputNotes)
 {
-    global $table_name;
     try {
         $connection = dbConnection();
-        $sql = "INSERT INTO sharedNotes(inputNotes) 
-            VALUES (:inputNotes)";
+        $noteTime  = date('H:i');
+        $authorUsername = $_SESSION["username"];
+        $sql = "INSERT INTO sharedNotes(inputNotes, noteTime, authorUsername) 
+            VALUES (:inputNotes, :noteTime, :authorUsername)";
 
         $preparedSql = $connection->prepare($sql) or die("Свързването е неуспешно!" . "<br>");
         $preparedSql->bindParam(':inputNotes', $inputNotes);
+        $preparedSql->bindParam(':noteTime', $noteTime);
+        $preparedSql->bindParam(':authorUsername', $authorUsername);
 
         if ($inputNotes != '') {
             $preparedSql->execute() or die("Неуспешно изпълнение на SQL заявката!" . "<br>");
@@ -33,14 +40,17 @@ function printContentOfTable()
 
         $connection = dbConnection();
         $sql = "SELECT * FROM {$table_name}";
-        $counter = 1;
+        /*$counter = 1;*/
 
         $result = $connection->prepare($sql) or die("Failed to prepare select sql query.");
         $result->execute() or die("Failed to execute select sql query.");
 
         while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-            echo "<div class=\"counter\">$counter. </div>" . $row["inputNotes"] . "<br/><br/>";
-            $counter++;
+            $noteTime = $row["noteTime"];
+            $sessionUsername = $row["authorUsername"];
+            /*echo "<div class=\"counter\">$counter. </div>" . $row["inputNotes"] . "<br/><br/>";*/
+            echo "<div class=\"counter\">$noteTime</div>" . "<div class=\"sessionUN\">$sessionUsername</div>" . "<div class=\"counter colon\">: </div>". $row["inputNotes"] . "<br/><br/>";
+            /*$counter++;*/
         }
 
         $connection = null;
--- sql/sharedNotes.sql
+++ sql/sharedNotes.sql
@@ -3,5 +3,7 @@ CREATE DATABASE IF NOT EXISTS `presentationCalendar`;
 USE `presentationCalendar`;
 
 CREATE TABLE IF NOT EXISTS `sharednotes` (
-  `inputNotes` longtext NOT NULL
+  `inputNotes` longtext NOT NULL,
+  `noteTime` text NOT NULL,
+  `authorUsername` text NOT NULL
 );
