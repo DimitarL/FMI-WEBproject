8599544f6f88a038e186ee745e069e2c2d31208c
Alexandra Prazova <56827661+aprazova@users.noreply.github.com>
2020-07-05 20:14:41 +0300

    Changing db structure and fill in the tables (#23)
    * Changing db structure and fill in the tables
    
    * Adding config file
    
    * Adding presentationLink column in presentationsTable

---
+++ php/config.php
@@ -0,0 +1,16 @@
+<?php
+
+// DB connection
+$config=array(
+    'DB_HOST'=>'localhost',
+    'DB_USERNAME'=>'root',
+    'DB_PASSWORD'=>'',
+    'DB_DATABASE'=>'presentationcalendar'
+);
+
+// Presentation files
+$presentation = array(
+    "PRESENTATION_PATH" => "../updates/presetations",
+    "INVITATION_PATH" => "../updates/invitations"
+);
+?>
--- php/db_connection.php
+++ php/db_connection.php
@@ -1,11 +1,16 @@
 <?php
+
+require_once "config.php";
+
 function dbConnection(){
 
-    $host = "localhost";
-    $dbname = "presentationcalendar";
-    $username = "root";
-    $pass = "";
+    global $config;
 
+    $host = $config['DB_HOST'];
+    $dbname = $config['DB_DATABASE'];
+    $username = $config['DB_USERNAME'];
+    $pass = $config['DB_PASSWORD'];
+    
     try{
         $connection = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $pass, [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"]);
         return $connection;
