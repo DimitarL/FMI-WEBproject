c7a678c8b88fc0225fdc13af5f834cb56bb35433
aprazova <alexandra.prazova@gmail.com>
2020-07-05 15:29:52 +0300

    Changing db structure and fill in the tables

---
+++ sql/create_tables.sql
@@ -0,0 +1,81 @@
+CREATE DATABASE IF NOT EXISTS `presentationCalendar`;
+
+USE `presentationCalendar`;
+
+CREATE TABLE IF NOT EXISTS dates(
+    timeDate timestamp PRIMARY KEY DEFAULT CURRENT_TIMESTAMP,
+    hasPresentation boolean DEFAULT false,
+    timeEnd timestamp DEFAULT CURRENT_TIMESTAMP,
+    duration int NOT NULL,
+    room varchar(64) NOT NULL,
+    day INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS students (
+    username VARCHAR(64) PRIMARY KEY,
+    firstName VARCHAR(64) NOT NULL,
+    lastName VARCHAR(64) NOT NULL,
+    password VARCHAR(64) NOT NULL,
+    course tinyint  NOT NULL, 
+    specialty VARCHAR(128) NOT NULL, 
+    facultyNumber INT NOT NULL, 
+    groupNumber SMALLINT UNSIGNED NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS lectors (
+    username VARCHAR(64) PRIMARY KEY,
+    firstName VARCHAR(64) NOT NULL,
+    lastName VARCHAR(64) NOT NULL,
+    password VARCHAR(64) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS topicsInfo (
+    topicId INT PRIMARY KEY,
+    topic VARCHAR(124),
+    presentationPath VARCHAR(64) NOT NULL,
+    invitationPath VARCHAR(64) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS present (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(64), 
+    topicId INT NOT NULL,
+    timeIn timestamp DEFAULT CURRENT_TIMESTAMP,
+    timeOut timestamp NULL,
+    FOREIGN KEY (username)
+    REFERENCES students(username),
+    FOREIGN KEY (topicId)
+    REFERENCES topicsInfo(topicId)    
+);
+
+CREATE TABLE IF NOT EXISTS presentations (
+    topicId INT,
+    username VARCHAR(64),
+    timeDate timestamp NULL,
+    CONSTRAINT fkDate FOREIGN KEY (timeDate)
+    REFERENCES dates(timeDate) ON UPDATE CASCADE,
+    CONSTRAINT fkStudent FOREIGN KEY (username)
+    REFERENCES students(username) ON UPDATE CASCADE,
+    CONSTRAINT fkTopicId FOREIGN KEY (topicId)
+    REFERENCES topicsInfo(topicId),  
+    PRIMARY KEY(topicId, username)
+);
+
+CREATE TABLE IF NOT EXISTS sharedNotes (
+  inputNotes longtext NOT NULL,
+  noteTime text NOT NULL,
+  authorUsername text NOT NULL,
+  topicId INT NOT NULL,
+  FOREIGN KEY (topicId)
+  REFERENCES topicsInfo(topicId)  
+); 
+
+SET GLOBAL event_scheduler = ON;
+
+drop event if exists delete_shared_notes;
+
+CREATE EVENT delete_shared_notes 
+ON SCHEDULE EVERY 1 DAY 
+STARTS TIMESTAMP (CURDATE() + INTERVAL 1 DAY, '00:00:01') DO DELETE FROM sharedNotes;
+
+show events; 
---
+++ sql/fill_db.sql
@@ -0,0 +1,97 @@
+-- STUDENTS
+INSERT INTO students(firstName, lastName, username, password, course, specialty, facultyNumber, groupNumber)
+VALUES ('Александър', 'Симеонов', 'asimeonov', '$2y$10$Yy.RsKq07b4JfL.krjJbvu4JwOV.DPQwMm2fFEEWJUT.XvGxub1ze', 3, 'Софтуерно инженерство', 78412, 1); /*pass = asimeonov*/
+
+INSERT INTO students(firstName, lastName, username, password, course, specialty, facultyNumber, groupNumber)
+VALUES ('Мартина', 'Александрова', 'maleksandrova', '$2y$10$AYdC1asECjJwNQYpQkgAnOp5R0qlbnuDDRlG5f/1daQeclvVSnXMO', 3, 'Софтуерно инженерство', 78416, 1); /*pass = maleksandrova*/
+
+INSERT INTO students(firstName, lastName, username, password, course, specialty, facultyNumber, groupNumber)
+VALUES ('Василена', 'Иванова', 'vivanova', '$2y$10$vvXsifXAHiHp35dcpOV8eOBIPN0KGbtZX2QS25.o7ZVDO9aQ8PTIC', 3, 'Софтуерно инженерство', 78417, 2); /*pass = vivanova*/
+
+INSERT INTO students(firstName, lastName, username, password, course, specialty, facultyNumber, groupNumber)
+VALUES ('Виктор', 'Мишев', 'vmishev', '$2y$10$bl8Q.M6TcSSdJz9smTHbzuXN0AvgFzXc5ZL7CzQ.GHWtMrD4xYSdW', 3, 'Софтуерно инженерство', 78419, 2); /*pass = vmishev*/
+
+INSERT INTO students(firstName, lastName, username, password, course, specialty, facultyNumber, groupNumber)
+VALUES ('Ивана', 'Симеонова', 'isimeonova', '$2y$10$qw3Y60GJaJ/9hzzhLqtuQu33Mw4qJg7c5raA2cdaqYTSg4IJyZ2bC', 3, 'Софтуерно инженерство', 78502, 1); /*pass = isimeonova*/
+
+INSERT INTO students(firstName, lastName, username, password, course, specialty, facultyNumber, groupNumber)
+VALUES ('Иван', 'Иванов', 'iivanov', '$2y$10$FDu9vwVDnamTHepOhanNAOKkURtH44VfuZhOlQvtB5ZRiIm0WzLjS', 3, 'Софтуерно инженерство', 78423, 2); /*pass = iivanov*/
+
+INSERT INTO students(firstName, lastName, username, password, course, specialty, facultyNumber, groupNumber)
+VALUES ('Маргарита', 'Менчева', 'mmencheva', '$2y$10$5094juCXnLn3O9oLm3YQYOPT3y1jJRyLus1wQQdJ0MkULYz.214Cy', 3, 'Софтуерно инженерство', 78432, 2); /*pass = mmencheva*/
+
+INSERT INTO students(firstName, lastName, username, password, course, specialty, facultyNumber, groupNumber)
+VALUES ('Милен', 'Алексиев', 'maleksiev', '$2y$10$UfcHFpuMqCwhBzQVc3cNeOCBegLSiPCL14QBGSts07DXDkWka8Jwu', 3, 'Софтуерно инженерство', 78433, 2); /*pass = maleksiev*/
+
+INSERT INTO students(firstName, lastName, username, password, course, specialty, facultyNumber, groupNumber)
+VALUES ('Доротея', 'Димитрова', 'dodimitrova', '$2y$10$/GMLOjU/BuoimSqX.lNywurwAPtYVh7VcF093Fh3NaOQboQ48lhAW', 3, 'Софтуерно инженерство', 78464, 1); /*pass = dodimitrova*/
+
+INSERT INTO students(firstName, lastName, username, password, course, specialty, facultyNumber, groupNumber)
+VALUES ('Дейвид', 'Денков', 'ddenkov', '$2y$10$7bzbODfsGoTlmee.279wOuqfdfS4PXBrHMP6pWcd5o.KhfHKwPqGG', 3, 'Софтуерно инженерство', 78468, 2); /*pass = ddenkov*/
+
+INSERT INTO students(firstName, lastName, username, password, course, specialty, facultyNumber, groupNumber)
+VALUES ('Цветелина', 'Нончева', 'cvnoncheva', '$2y$10$TRmuH4gwfOq7/v5vh//Wl.WYMYe450NNRjjdNLdZxw9qawkJny7fG', 3, 'Софтуерно инженерство', 78481, 1); /*pass = cvnoncheva*/
+
+INSERT INTO students(firstName, lastName, username, password, course, specialty, facultyNumber, groupNumber)
+VALUES ('Симон', 'Генчев', 'sgenchev', '$2y$10$4EUuhL/lIk//0i5vJZRVGOCbmJBPlxybrP4Fcq9/hd3e8lbj8TBSi', 3, 'Софтуерно инженерство', 78482, 2); /*pass = sgenchev*/
+
+INSERT INTO students(firstName, lastName, username, password, course, specialty, facultyNumber, groupNumber)
+VALUES ('Мартинела', 'Михалкова', 'mmihalkova', '$2y$10$CCGb8CpVt/nxzFADbFH2DOVVOg8vO4TAEiG1yheR6L1bzUlAzGzse', 3, 'Софтуерно инженерство', 78489, 1); /*pass = mmihalkova*/
+
+INSERT INTO students(firstName, lastName, username, password, course, specialty, facultyNumber, groupNumber)
+VALUES ('Йордан', 'Цончев', 'yochonchev', '$2y$10$XdPIr1eWcjPoRg0dmpg9oOxcd7Q6S5Qj.iRxrxYI315hdt3c6E4XG', 3, 'Софтуерно инженерство', 78490, 1); /*pass = yochonchev*/
+
+INSERT INTO students(firstName, lastName, username, password, course, specialty, facultyNumber, groupNumber)
+VALUES ('Здравко', 'Конев', 'zkonev', '$2y$10$WTkenPrghpJKzukTEZmTYu7hezyLzSx5UwumK.wSKlqfMo9Vq0dxW', 3, 'Софтуерно инженерство', 78497, 1); /*pass = zkonev*/
+
+-- LECTORS
+INSERT INTO lectors (firstName, lastName, username, password)
+VALUES ('Иван', 'Василев', 'ivvasilev', '$2y$10$/Xc4cPsrd4WoB7UWzzXc.OPx..tuX1GNoWNmzcCGyZN33C59hGtkK'); /*pass = ivvasilev*/
+
+INSERT INTO lectors (firstName, lastName, username, password)
+VALUES ('Марио', 'Миков', 'mamikov', '$2y$10$8k/gxdL2kqfs..2Mm1seger6oJKdjT3nMJaSkPT/LMx2KKf.Nqdsy'); /*pass = mamikov*/
+
+INSERT INTO lectors (firstName, lastName, username, password)
+VALUES ('Георги', 'Георгиев', 'gegeorgiev', '$2y$10$2dH4E82ZVXL0gyqkMWmCeu1GyNI.m.0kKDfJhhD1lVIK5euJ2gaEa'); /*pass = gegeorgiev*/
+
+INSERT INTO lectors (firstName, lastName, username, password)
+VALUES ('Симона', 'Генева', 'sigeneva', '$2y$10$/WMDqzAoYE..Omp5KbV1be8PlF5S9/.r9BitBrAkYiXmKAv8cue/S'); /*pass = sigeneva*/
+
+INSERT INTO lectors (firstName, lastName, username, password)
+VALUES ('Мартина', 'Минева', 'mamineva', '$2y$10$okneBzAVTXaiUA/Bqq21YuNvRY.LRy82naVdrw1vEOSJw9vEDIO/C'); /*pass = mamineva*/
+
+-- TOPICS
+INSERT INTO topicsInfo(topicId, topic, presentationPath, invitationPath)
+VALUES ( 1 , 'Google - Web Performance Best Practices', '../uploads/presentations/78412_presentation.pptx', '../uploads/invitations/78412_invitations.png'),
+( 2 , 'HTML5 - част 1 семантични тагове, тагове за форми. Примери.', '../uploads/presentations/78416_presentation.pptx', '../uploads/invitations/78416_invitations.png'),
+( 3 , 'Latex - сравнение с HTML', '../uploads/presentations/78417_presentation.pptx', '../uploads/invitations/78417_invitations.png'),
+( 4 , 'CSS: стилове, класове, селектори', '../uploads/presentations/78419_presentation.pptx', '../uploads/invitations/78419_invitations.png'),
+( 5 , 'CSS: layouts, box model', '../uploads/presentations/78502_presentation.pptx', '../uploads/invitations/78502_invitations.png'),
+( 6 , 'CSS: layouts, flexbox', '../uploads/presentations/78423_presentation.pptx', '../uploads/invitations/78423_invitations.png'),
+( 7 , 'Анимации с CSS с използване на трансформации', '../uploads/presentations/78432_presentation.pptx', '../uploads/invitations/78432_invitations.png'),
+( 8 , 'Еммет синтаксис', '../uploads/presentations/78433_presentation.pptx', '../uploads/invitations/78433_invitations.png'),
+( 9 , 'DOM дърво, обхождане и манипулация', '../uploads/presentations/78464_presentation.pptx', '../uploads/invitations/78464_invitations.png'),
+( 10 , 'Работа със сесии и cookies (от страна насървъра и клиента). Local Storage API', '../uploads/presentations/78468_presentation.pptx', '../uploads/invitations/78468_invitations.png'),
+( 11 , 'HTML мета-тагове и SEO', '../uploads/presentations/78481_presentation.pptx', '../uploads/invitations/78481_invitations.png'),
+( 12 , 'Протокол HTTP версия 2.0+', '../uploads/presentations/78482_presentation.pptx', '../uploads/invitations/78482_invitations.png'),
+( 13 , 'CSS Preprocessor/LESS и SASS', '../uploads/presentations/78489_presentation.pptx', '../uploads/invitations/78489_invitations.png'),
+( 14 , 'External Data Representation (XDR)', '../uploads/presentations/78490_presentation.pptx', '../uploads/invitations/78490_invitations.png'),
+( 15 , 'Svelte web tool', '../uploads/presentations/78497_presentation.pptx', '../uploads/invitations/78497_invitations.png');
+
+-- PRESENTATIONS
+INSERT INTO presentations(username, topicId)
+VALUES ('asimeonov',1),
+('maleksandrova',2),
+('vivanova', 3),
+('vmishev', 4),
+('isimeonova', 5),
+('iivanov', 6),
+('mmencheva', 7),
+('maleksiev', 8),
+('dodimitrova', 9),
+('ddenkov', 10),
+('cvnoncheva', 11),
+('sgenchev', 12),
+('mmihalkova', 13),
+('yochonchev', 14),
+('zkonev', 15);
