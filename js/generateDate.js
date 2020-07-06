12a92bc34192035a7b7a08e9b54b2c922f2b25f6
dkrusteva1 <dessynceto@abv.bg>
2020-07-06 14:45:37 +0300

    Add option for bigger duration when generating dates

--- js/generateDate.js
+++ js/generateDate.js
@@ -70,7 +70,7 @@ function getMinutes(d) {
 }
 
 function validateDuration() {
-    if (document.getElementById("duration").value < 3 || document.getElementById("duration").value > 10) {
+    if (document.getElementById("duration").value < 3 || document.getElementById("duration").value > 180) {
         document.getElementById("duration").style.backgroundColor = '#fba';
         document.getElementById("error").innerText = "Продължителността е от 3 до 10 минути.";
         return false;
