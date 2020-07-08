b17ebbba434579802695f84242866e0890ed1dea
DimitarL <miteto66@gmail.com>
2020-07-08 21:59:44 +0300

    Fixed errors

--- sql/sharedNotes.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS `presentationCalendar`;
-
-USE `presentationCalendar`;
-
-CREATE TABLE IF NOT EXISTS `sharednotes` (
-  `inputNotes` longtext NOT NULL,
-  `noteTime` text NOT NULL,
-  `authorUsername` text NOT NULL
-);
