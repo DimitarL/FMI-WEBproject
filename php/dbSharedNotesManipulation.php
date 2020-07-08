eaada17b13a946804b2f2952a6aeb37bac2bf959
DimitarL <36930531+DimitarL@users.noreply.github.com>
2020-07-08 22:23:45 +0300

    Dimitar (#30)
    * Fixed errors
    
    * Fixed errors

--- php/dbSharedNotesManipulation.php
+++ php/dbSharedNotesManipulation.php
@@ -1,5 +1,5 @@
 <?php
-if(!session_id()){
+if (!session_id()) {
     session_start();
 }
 
@@ -8,8 +8,7 @@ include "db_connection.php";
 date_default_timezone_set('Europe/Sofia');
 $table_name = "sharedNotes";
 
-function insertInTable($inputNotes)
-{
+function insertInTable($inputNotes) {
     try {
         $connection = dbConnection();
         $authorUsername = $_SESSION["username"];
@@ -17,24 +16,23 @@ function insertInTable($inputNotes)
         $topicId = getCurrentTopicId();
 
         $sql = "INSERT INTO sharedNotes(inputNotes, noteTime, authorUsername, topicId)
-            VALUES (:inputNotes, :noteTime, :authorUsername, :topicId)";
+                VALUES (:inputNotes, :noteTime, :authorUsername, :topicId)";
  
         $preparedSql = $connection->prepare($sql) or die("Свързването е неуспешно!" . "<br>");
         $preparedSql->bindParam(':inputNotes', $inputNotes);
         $preparedSql->bindParam(':noteTime', $noteTime);
         $preparedSql->bindParam(':authorUsername', $authorUsername);
         $preparedSql->bindParam(':topicId', $topicId);
- 
+
         $preparedSql->execute() or die("Неуспешно изпълнение на SQL заявката!" . "<br>");
         echo "Данните са добавени успешно!" . "<br>";
         $connection = null;
-        
     } catch (PDOException $error) {
         echo "Възникна грешка с добавянето на бележката. Моля, опитайте пак по-късно.";
     }
 }
- 
-function getCurrentTopicId(){
+
+function getCurrentTopicId() {
     try {
         $connection = dbConnection();
         $currentTime = date('Y-m-d H:i');
@@ -47,35 +45,31 @@ function getCurrentTopicId(){
         $result->bindParam(':currentTime', $currentTime);
         $result->execute() or die("Failed to execute select sql query.");
         $row = $result->fetch(PDO::FETCH_ASSOC);
-        
+
         $connection = null;
 
         return $row['topicId'];
     } catch (PDOException $error) {
-        echo "Неуспешен опит да се заредим номера на темата.";
-        return 10;
+        echo "Неуспешен опит да се зареди номера на темата.";
+        return 0;
     }
 }
 
-function printContentOfTable()
-{
+function printContentOfTable() {
     global $table_name;
     try {
         echo "<div class=\"notesTitle\">Споделени бележки:</div>" . "<br/>";
 
         $connection = dbConnection();
         $sql = "SELECT * FROM {$table_name}";
-        /*$counter = 1;*/
-
-        $result = $connection->prepare($sql) or die("Failed to prepare select sql query.");
-        $result->execute() or die("Failed to execute select sql query.");
+        $result = $connection->prepare($sql) or die("Неуспешно изпълнение на sql заявката.");
+        $result->execute() or die("Неуспешно изпълнение на sql заявката.");
 
         while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
             $noteTime = $row["noteTime"];
             $sessionUsername = $row["authorUsername"];
-            /*echo "<div class=\"counter\">$counter. </div>" . $row["inputNotes"] . "<br/><br/>";*/
-            echo "<div class=\"counter\">$noteTime</div>" . "<div class=\"sessionUN\">$sessionUsername</div>" . "<div class=\"counter colon\">: </div>". $row["inputNotes"] . "<br/><br/>";
-            /*$counter++;*/
+            
+            echo "<div class=\"counter\">$noteTime</div>" . "<div class=\"sessionUN\">$sessionUsername</div>" . "<div class=\"counter colon\">: </div>" . $row["inputNotes"] . "<br/><br/>";
         }
 
         $connection = null;
@@ -84,9 +78,7 @@ function printContentOfTable()
     }
 }
 
-function downloadNotes()
-{
-    global $table_name;
+function downloadNotes() {
     try {
         $connection = dbConnection();
         $sql = "SELECT firstName, lastName, facultyNumber, t.topicId, t.topic, inputNotes, noteTime
@@ -97,13 +89,14 @@ function downloadNotes()
         ON s.username = sn.authorUsername
         ORDER BY noteTime;";
         $counter = 1;
-
         $result = $connection->prepare($sql) or die("Failed to prepare select sql query.");
         $result->execute() or die("Failed to execute select sql query.");
+        
         $fileOpen = fopen('../downloaded_Notes.txt', 'w');
 
         while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-            fputs($fileOpen, "Автор: ". $row['facultyNumber'] . ", " . $row['firstName'] . " " . $row['lastName'] . "\nИме на темата: " . $row['topic'] . "; Номер на темата: " . $row['topicId'] . "\n" . $counter . ". Бележка: " . $row['inputNotes'] . "\n\n");
+            fputs($fileOpen, "Автор: " . $row['facultyNumber'] . ", " . $row['firstName'] . " " . $row['lastName'] . "\nИме на темата: " . $row['topic'] . "; Номер на темата: " . $row['topicId'] . "\n" . $counter . ". Бележка: " . $row['inputNotes'] . "\n\n");
+
             $counter++;
         }
 
